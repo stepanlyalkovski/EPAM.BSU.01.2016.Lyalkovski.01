a1296a76dec342089f7542cb1ae008eac2e4af43
StepanLyalkovski <stepanlyalkovski@gmail.com>
2016-03-09 13:33:53 +0300

    Complete Task1

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1;
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== Sqrt testing ====");
+            Console.Write("number: ");
+            double number = double.Parse(Console.ReadLine());
+            Console.Write("n: ");
+            double n = double.Parse(Console.ReadLine());
+            Console.Write("precision: ");
+            double eps = double.Parse(Console.ReadLine());
+            double result = MathOperations.Sqrt(number, n, 0.0000001);
+            double test = Math.Pow(result, n);
+            Console.WriteLine("Sqrt result: " + result);
+            Console.WriteLine($"Comparing to Math.Pow:\n Number: {number}\n sqrt result after Math.Pow: {test}");
+        }
+    }
+}
---
+++ Task1/MathOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public static class MathOperations
+    {
+        public static double Sqrt(double a, double n, double eps = 0.0001)
+        {
+            if (a < 0)
+                throw new Exception("Can not sqrt a negative number");
+            if (eps < 0)
+                throw new ArgumentOutOfRangeException();
+            double x = 1;
+            while (true)
+            {
+                double val = Math.Pow(x, n);
+                if (Math.Abs(val - a) <= eps)
+                    return x;
+                x = (1 / n) * ((n - 1) * x + a / Math.Pow(x, (int)n - 1));
+            }
+        }
+    }
+}
