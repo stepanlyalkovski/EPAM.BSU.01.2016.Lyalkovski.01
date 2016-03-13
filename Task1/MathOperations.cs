35905b2c796cab688e2c0eb57cc07c64e2acb9d5
StepanLyalkovski <stepanlyalkovski@gmail.com>
2016-03-13 18:37:53 +0300

    refactor Sqrt for readability

--- Task1/MathOperations.cs
+++ Task1/MathOperations.cs
@@ -8,19 +8,21 @@ namespace Task1
 {
     public static class MathOperations
     {
-        public static double Sqrt(double a, double n, double eps = 0.0001)
+        public static double Sqrt(double number, int power, double eps = 0.0001)
         {
-            if (a < 0)
-                throw new Exception("Can not sqrt a negative number");
+            if (number < 0)
+                throw new Exception("Can not sqrt number negative number");
             if (eps < 0)
                 throw new ArgumentOutOfRangeException();
-            double x = 1;
+
+            double result = 1;
+
             while (true)
             {
-                double val = Math.Pow(x, n);
-                if (Math.Abs(val - a) <= eps)
-                    return x;
-                x = (1 / n) * ((n - 1) * x + a / Math.Pow(x, n - 1));
+                double powResult = Math.Pow(result, power);
+                if (Math.Abs(powResult - number) <= eps)
+                    return result;
+                result = (1.0 / power) * ((power - 1) * result + number / Math.Pow(result, power - 1));
             }
         }
     }
